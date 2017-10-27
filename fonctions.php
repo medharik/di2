5894a640710aabc5f8994802857422316d1791c6
Harik med <hamouda.form@gmail.com>
2017-10-27 10:44:45 +0100

    form 3 : label , html5 , ...

--- fonctions.php
+++ fonctions.php
@@ -1,13 +1,16 @@
 <?php 
+  $adresse=connecter_db();
 function connecter_db()
 {
-	$adresse=mysqli_connect("localhost", "root", "", "dbcesa2017tp") or die("erreur de connexion à la bd") ;
+	 $adresse=mysqli_connect("localhost", "root", "", "dbcesa2017tp") or die("erreur de connexion à la bd") ;
 return $adresse;
 }
 //ajout de produit dans la base de données : 
 function ajouter_produit($nom,$prix)
 {//connection à la bd
-	$adresse=connecter_db();
+	global $adresse;
+
+	//$adresse=connecter_db();
 	//formuler la requete  sql (crud : create , read, update et delete)
 	$sql=sprintf("insert into produit(nom,prix) values('%s','%f')",e($nom),$prix);
 	//executé la requete sql
@@ -17,17 +20,17 @@ function ajouter_produit($nom,$prix)
 }
 // suppression de pruduit de la bd
 function supprimer_produit($id)
-{	$adresse=connecter_db();
+{	//$adresse=connecter_db();
 	$sql=sprintf("delete from produit where id=%d)",$id);
 	
-	mysqli_query($adresse, $sql) or die("Erreur de suppression ".mysqli_error($adresse));
+	mysqli_query($GLOBALS['adresse'], $sql) or die("Erreur de suppression ".mysqli_error($adresse));
 	;
 	
 }
 
 // modification  de pruduit de la bd
 function modifier_produit($id,$new_nom,$new_prix)
-{	$adresse=connecter_db();
+{	//$adresse=connecter_db();
 	$sql=sprintf("update produit set 
 nom='%s',
 prix='%f'
@@ -41,7 +44,7 @@ where id=%d
 
 //selection des données depuis la bd
 function get_all_produit()
-{	$adresse=connecter_db();
+{	//$adresse=connecter_db();
 	$sql=sprintf("select * from produit order by id asc");
 	
 	$resultat=mysqli_query($adresse, $sql) or die("Erreur de selection ".mysqli_error($adresse));
@@ -51,16 +54,16 @@ function get_all_produit()
 //fonction generique de lecture depuis une bd
 function get_all($table)
 {
-	$adresse=connecter_db();
+	//$adresse=connecter_db();
 	$sql=sprintf("select * from $table order by id desc");
 	
-	$resultat=mysqli_query($adresse, $sql);
+	$resultat=mysqli_query($GLOBALS['adresse'], $sql);
 	return $resultat;
 }
 //fontion generique qui recupere par l'id
 function get_by_id($id,$table)
 {
-	$adresse=connecter_db();
+	//$adresse=connecter_db();
 	$sql=sprintf("select * from $table 
 where id=%d		",$id);
 	
@@ -71,17 +74,17 @@ where id=%d		",$id);
 }
 // suppression générique d'une table de la bd
 function supprimer($id,$table)
-{	$adresse=connecter_db();
+{	//$adresse=connecter_db();
 	$sql=sprintf(" delete from $table where id=%d ",$id);
 	
-	mysqli_query($adresse, $sql) 
-	or die("Erreur de suppression ".mysqli_error($adresse));
+	mysqli_query($GLOBALS['adresse'], $sql) 
+	or die("Erreur de suppression ".mysqli_error($GLOBALS['adresse']));
 	
 }
 
 function e($mot) 
 {
-	$adresse=connecter_db();
-return mysqli_real_escape_string($adresse, $mot);
+	//$adresse=connecter_db();
+return mysqli_real_escape_string($GLOBALS['adresse'], $mot);
 }
  ?>
---
+++ valide.js
@@ -0,0 +1,7 @@
+function valdier() {
+	e=document.getElementById('e');
+	me=/[a-z]{1,}[a-z0-9._]@[a-z]{1,}[.][1,3]$/
+if (me.test(e.value)) {
+
+}
+}
